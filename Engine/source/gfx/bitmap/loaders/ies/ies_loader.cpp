51be14645d9b4080c995312ee0c230a440cf23b5
Sven <68018116+SKBotNL@users.noreply.github.com>
2024-04-25 19:58:50 +0200

    Remove redundant include

--- Engine/source/gfx/bitmap/loaders/ies/ies_loader.cpp
+++ Engine/source/gfx/bitmap/loaders/ies/ies_loader.cpp
@@ -34,7 +34,6 @@
 // | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // +----------------------------------------------------------------------
-#include "ies_loader.h"
 #include <assert.h>
 #include <algorithm>
 #include <cstdint>
