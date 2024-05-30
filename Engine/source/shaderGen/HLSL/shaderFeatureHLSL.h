56e4484ff612a70456e2ebd1e6ffff4c1508c47e
AzaezelX <quillus@hotmail.com>
2024-05-30 17:29:42 -0500

    remove glowbin as it's own render pass

--- Engine/source/shaderGen/HLSL/shaderFeatureHLSL.h
+++ Engine/source/shaderGen/HLSL/shaderFeatureHLSL.h
@@ -555,11 +555,9 @@ public:
 class GlowMaskHLSL : public ShaderFeatureHLSL
 {
 public:
+
    void processPix(   Vector<ShaderComponent*> &componentList, 
                               const MaterialFeatureData &fd ) override;
-
-   Material::BlendOp getBlendOp() override { return Material::None; }
-
    String getName() override
    {
       return "Glow Mask";
